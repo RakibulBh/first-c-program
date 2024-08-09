23a9aa7bf6fdb38e0e0280620597589556225c8e
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-08-09 16:40:33 +0100

    Counting letters in string

--- main.c
+++ main.c
@@ -2,23 +2,11 @@
 /* print Fahrenheit-Celsius table
 for fahr = 0, 20, ..., 300; floating-point version */
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
-
 int main()
 {
-    printf("Fahrenheit-Celsius table\n");
-    for (float fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
+    double nc;
+    for (nc = 0; getchar() != EOF; ++nc)
     {
-        printf("%3.0f %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
-    }
-
-    int c;
-    c = getchar();
-    while (c != EOF)
-    {
-        putchar(c);
-        c = getchar();
+        printf("%.0f\n", nc);
     }
 }
