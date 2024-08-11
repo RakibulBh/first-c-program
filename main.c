62db91a93d56c3116a3bab68615588b6f7b24d6a
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-08-11 15:44:03 +0100

    Write a program that writes one word per line

--- main.c
+++ main.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
-/* print Fahrenheit-Celsius table
-for fahr = 0, 20, ..., 300; floating-point version */
 
 int main()
 {
     int c;
-    int previousBlank = 0; // This will track if the last character was a blank
-
     while ((c = getchar()) != EOF)
     {
-        if (c == ' ')
-        {
-            if (!previousBlank)
-            { // If the previous character wasn't a blank, print this one
-                putchar(c);
-                previousBlank = 1; // Now the previous character is a blank
-            }
-            // If the previous character was a blank, do nothing (skip this blank)
-        }
+        if (c == '\n' || c == ' ' || c == '\t')
+            putchar('\n');
         else
-        {
-            putchar(c);        // Print the current character
-            previousBlank = 0; // Reset, because the current character is not a blank
-        }
+            putchar(c);
     }
 
     return 0;
