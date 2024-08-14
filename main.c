a18379918a4e03f7946b05d00eda67f122ee7228
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-08-14 15:08:23 +0100

    Write a program to remove all comments from a C program. Don't forget to handle quoted strings and character constants properly. C comments don't nest.

--- main.c
+++ main.c
@@ -2,25 +2,57 @@
 
 int main()
 {
-     int c;
-    int char_count = 0;
+      int c;
+    bool is_comment = false;
+    int slash_count = 0;
+    char comments[100]; // Increased size to accommodate comments
+    int comment_num = 0;
+    int curr_comment_index = 0; // Changed to index for current comment
+
     while ((c = getchar()) != EOF)
     {
-        if (c == ' ' || c == '\n' || c == '\t')
+        if (c == '/')
         {
-            putchar(' ');
-            for (int i = 0; i < char_count; i++)
+            slash_count++;
+            if (slash_count == 2 && !is_comment)
             {
-                putchar('-');
-            };
-            char_count = 0;
-            putchar('\n');
+                is_comment = true;
+                slash_count = 0;
+                comments[comment_num++] = c; // Store the first '/'
+            }
+            else if (slash_count > 2 && is_comment)
+            {
+                comments[comment_num++] = c; // Store additional '/' characters in comment
+            }
+            else
+            {
+                // Not a comment, print the '/'
+                putchar(c);
+                slash_count = 0;
+            }
+        }
+        else if (is_comment && c == '\n')
+        {
+            // End of comment
+            is_comment = false;
+            comments[comment_num++] = c; // Store newline character in comment
+        }
+        else if (is_comment)
+        {
+            // Inside a comment, store characters
+            comments[comment_num++] = c;
         }
         else
         {
-            putchar(c);
-            char_count++;
+            // Not a comment, print the character
+            // putchar(c);
         }
+    }
+
+    // Print out all the stored comments after the code
+    for (int i = 0; i < comment_num; i++)
+    {
+        putchar(comments[i]);
     }
 
     return 0;
